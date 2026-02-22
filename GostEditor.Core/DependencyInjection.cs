25d28a18dc2b9fd9e955637645219c03ffc754ca
jota2069 <mkat4274@gmail.com>
2026-02-22 23:00:11 +0300

    feat(core): добавлен DependencyInjection

---
+++ GostEditor.Core/DependencyInjection.cs
@@ -0,0 +1,17 @@
+using GostEditor.Core.Interfaces;
+using GostEditor.Core.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GostEditor.Core;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddGostEditorCore(
+        this IServiceCollection services)
+    {
+        services.AddSingleton<ITextNormalizerService, TextNormalizerService>();
+        services.AddSingleton<ICodeParserService, CodeParserService>();
+        services.AddSingleton<IDocumentService, DocumentService>();
+        return services;
+    }
+}
